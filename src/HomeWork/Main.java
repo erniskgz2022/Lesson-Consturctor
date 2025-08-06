147f1674db907d5521825ce8d7100ae4ff49d5c3
ernisbcknd <ernis.turgunbaiuulu.alt@gmail.com>
2025-08-06 16:34:01 +0300

    update

---
+++ src/HomeWork/Main.java
@@ -0,0 +1,11 @@
+package HomeWork;
+
+public class Main {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass("ERNIS","TURGUNBAI UULU",24);
+        MyClass myClass1 = new MyClass(new String[]{"Java", "Spring","Git","Database"}, "Плов");
+
+        myClass.getInfo();
+        myClass1.getInfo2();
+    }
+}
---
+++ src/HomeWork/MyClass.java
@@ -0,0 +1,44 @@
+package HomeWork;
+
+import java.util.Arrays;
+
+public class MyClass {
+    String name;
+    String surname;
+    int age;
+    String [] lessons;
+    String foods;
+
+    public MyClass(){
+
+    }
+
+    public MyClass (String name, String surname,int age){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public MyClass (String  [] lessons, String foods){
+        this.lessons = lessons;
+        this.foods = foods;
+    }
+
+    public void getInfo(){
+        System.out.println(
+                "First Name: " + name + "\n" +
+                "SurName: " + surname + "\n" +
+                "Age: " + age
+        );
+
+    }
+    public void getInfo2(){
+        System.out.println(
+                        "Lessons : " + Arrays.toString(lessons) + "\n" +
+                        "Foods: " + foods
+        );
+
+    }
+
+}
+
